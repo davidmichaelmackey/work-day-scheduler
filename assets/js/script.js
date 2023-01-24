3913770b157c0970ca4a7de8c3fe426d3dfeed04
David Mackey <davidmackey92@gmail.com>
2023-01-24 06:54:24 -0600

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ const storage = `timeBlocks`;
 // 
 // display current day
 // 
-$(`#currentDay`).text(today.format(`dddd, MMM Do`));
+$(`#currentDay`).text(today.format(`dddd, MMM D`));
 // 
 // check for existing localStorage
 // if from current date > load
