51462fa2987549839c47e7f30521327ca6f26232
David Mackey <davidmackey92@gmail.com>
2023-01-24 05:49:44 -0600

    localStorage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,31 @@
 let today = moment();
 let currentHour = today.format(`H`);
 let timeBlocks = {};
-const storageKeyName = `timeBlocks`;
-
-// display today's date
+const storage = `timeBlocks`;
+// 
+// display current day
+// 
 $(`#currentDay`).text(today.format(`dddd, MMM Do`));
+// 
+// check for existing localStorage
+// if from current date > load
+// if not > clear
+// 
+function initLocalStorage() {
+  if (localStorage.getItem(storage)) {
+    timeBlocks = JSON.parse(localStorage.getItem(storage));
+    if (timeBlocks.currentDate == today.format(`MM/DD/YYYY`)) {
+      return;
+    } else {
+      localStorage.removeItem(storage);
+    }
+  }
+  // 
+  // fill timeblock array
+  // 
+  timeBlocks.currentDate = today.format(`MM/DD/YYYY`);
+  for (let hour = 9; hour <= 17; hour++) {
+    timeBlocks[hour] = "";
+  }
+  localStorage.setItem(storage, JSON.stringify(timeBlocks));
+}
